700e6930ceffcdc0825a4fa448106ca2700ff611
Stanley <benaiahjuarez@gmail.com>
2026-05-10 14:36:06 -0700

    made poster select reason

--- GodotWildJam93/Globals/GlobalVars.gd
+++ GodotWildJam93/Globals/GlobalVars.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var money_in_vault: int = 10000
+var current_real_bounty: int
--- GodotWildJam93/reason_generator.gd
+++ GodotWildJam93/reason_generator.gd
@@ -1,16 +1,51 @@
 extends Control
 
-@export var Discription: Array[String]
-@export var Blank1: Array[String]
-@onready var label: Label = $Label
+@export var Discription: Dictionary[int, String]
+@export var Blank1: Dictionary[int, String]
+@onready var label: Button = $Label
+@onready var menu_select: Control = $MenuSelect
 
 func _ready() -> void:
 	SignalHub.Bounty_palced.connect(on_done_clicked)
-	Blank1.shuffle()
-	Discription.shuffle()
-	label.text = str(Discription.get(0)) + " " + str(Blank1.get(0))
 
 func on_done_clicked(bounty: int, Picture: int, name: String):
-	Blank1.shuffle()
-	Discription.shuffle()
-	label.text = str(Discription.get(0)) + " " + str(Blank1.get(0))
+	label.text = ""
+
+
+func get_real_Bounty(dis: int, name: int):
+	var Current_bounty: int
+	if dis == 1:
+		Current_bounty = 500
+	if dis == 2:
+		Current_bounty = 200
+	if dis == 3:
+		Current_bounty = 600
+	if dis == 4:
+		Current_bounty = 100
+	if dis == 5:
+		Current_bounty = 400
+
+func _on_stealing_pressed() -> void:
+	label.text = "for stealing resources"
+
+func _on_not_working_pressed() -> void:
+	label.text = "for not working"
+
+func _on_working_pressed() -> void:
+	label.text = "for working"
+
+func _on_reason_magic_pressed() -> void:
+	label.text = "for the use of Reason magic"
+
+func _on_pleasure_magic_pressed() -> void:
+	label.text = "for the use of pleasure magic"
+
+func _on_moral_magic_pressed() -> void:
+	label.text = "for the use of moral magic"
+
+
+func _on_label_pressed() -> void:
+	if menu_select.visible == false:
+		menu_select.visible = true
+	elif menu_select.visible == true:
+		menu_select.visible = false
